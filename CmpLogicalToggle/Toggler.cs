bf1206bf81d4f77cc402536e690898d7c6d32ba7
SweptThrone <im@sweptthr.one>
2025-04-09 15:09:57 -0500

    add warning/confirmation

--- CmpLogicalToggle/Toggler.cs
+++ CmpLogicalToggle/Toggler.cs
@@ -1,30 +1,35 @@
-﻿using Microsoft.Win32;
+using Microsoft.Win32;
 using System.Diagnostics;
 
 namespace CmpLogicalToggle {
     internal class CmpLogicalToggler {
         static void Main() {
-            object? currVal = Registry.GetValue( "HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Policies\\Explorer", "NoStrCmpLogical", 0 );
+            Console.Write( "WARNING:  Explorer.exe and all of its windows will be closed.  Proceed? (y/n) >> " );
+            string choice = ( Console.ReadLine() ?? "n" ).ToLower();
 
-            if ( currVal == null ) {
-                currVal = 0;
-            }
+            if ( choice == "y" ) {
+                object? currVal = Registry.GetValue( "HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Policies\\Explorer", "NoStrCmpLogical", 0 );
 
-            int intVal = ( int )currVal;
+                if ( currVal == null ) {
+                    currVal = 0;
+                }
 
-            if ( intVal == 1 ) {
-                Registry.SetValue( "HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Policies\\Explorer", "NoStrCmpLogical", 0 );
-                Console.WriteLine( "NoStrCmpLogical has been turned OFF." );
-            } else {
-                Registry.SetValue( "HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Policies\\Explorer", "NoStrCmpLogical", 1 );
-                Console.WriteLine( "NoStrCmpLogical has been turned ON." );
-            }
+                int intVal = ( int )currVal;
 
-            Process proc = Process.GetProcessesByName( "explorer" )[ 0 ];
-            proc.Kill();
-            if ( proc.MainModule != null ) {
-                string path = proc.MainModule.FileName;
-                Process.Start( path );
+                if ( intVal == 1 ) {
+                    Registry.SetValue( "HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Policies\\Explorer", "NoStrCmpLogical", 0 );
+                    Console.WriteLine( "NoStrCmpLogical has been turned OFF." );
+                } else {
+                    Registry.SetValue( "HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Policies\\Explorer", "NoStrCmpLogical", 1 );
+                    Console.WriteLine( "NoStrCmpLogical has been turned ON." );
+                }
+
+                Process proc = Process.GetProcessesByName( "explorer" )[ 0 ];
+                proc.Kill();
+                if ( proc.MainModule != null ) {
+                    string path = proc.MainModule.FileName;
+                    Process.Start( path );
+                }
             }
 
             Console.WriteLine( "Have a nice day :)" );
